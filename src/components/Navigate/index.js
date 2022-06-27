92d537595b693b87babbbe5bb724c43ba5bf2ab0
melaniemdm <melanieborja.pro@gmail.com>
2022-06-27 22:14:22 +0200

    fix(css & js) navigate

--- src/components/Navigate/index.js
+++ src/components/Navigate/index.js
@@ -5,13 +5,13 @@ import {Link} from 'react-router-dom';
 function Navigate(){
     return(
         <div className="containerNav">
-            
+     <div class="container topBotomBordersOut">     
 <nav className="navigate">
       <Link to="/accueil " className="accueil">Accueil</Link> 
       <Link to="/presentation " className="presentation">Présentation</Link>
       <Link to="/news " className="news">Les news de la fondation</Link>
       <Link to="/aider " className="aider">Nous aider</Link>
-    </nav>
+    </nav></div>  
         </div>
     )
 }
