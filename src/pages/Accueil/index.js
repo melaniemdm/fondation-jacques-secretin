72048de9f769d06c126db79babfcf398d955bcb2
melaniemdm <melanieborja.pro@gmail.com>
2022-06-28 15:40:54 +0200

    feature(js & css) presentationF add(img)

--- src/pages/Accueil/index.js
+++ src/pages/Accueil/index.js
@@ -1,12 +1,13 @@
 import ScrollToTop from "react-scroll-to-top";
 import Banner from "../../components/Banner";
+import Presentation from '../../components/Presentation';
 import { jacquesSecretinImg} from '../../datas/imgJacquesS';
 
 function Accueil() {
     return (
       <div>
         <Banner data={jacquesSecretinImg.picturesBanner}/>
-        
+        <Presentation/>
         <ScrollToTop />
       </div>
     );
