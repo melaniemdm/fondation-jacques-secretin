673109a121a0ef370903640bd0cb2a7b418739c3
melaniemdm <melanieborja.pro@gmail.com>
2022-06-29 16:25:57 +0200

    fix(js & css) new  & presentation

--- src/components/NewsFondation/index.js
+++ src/components/NewsFondation/index.js
@@ -4,24 +4,30 @@ import './style.scss';
 
  function NewsFondation(){
     return(<div className='containerNews'>
-<div className="titleNew">Les news de la Fondation</div>
-<div className="containerTxtContenu">
-
-
-<div><strong> Qu'est-ce que le Lorem Ipsum?</strong><br></br>
-Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</div><br></br>
-<div><strong>D'où vient-il?</strong><br></br>
-Contrairement à une opinion répandue, le Lorem Ipsum n'est pas simplement du texte aléatoire. Il trouve ses racines dans une oeuvre de la littérature latine classique datant de 45 av. J.-C., le rendant vieux de 2000 ans. Un professeur du Hampden-Sydney College, en Virginie, s'est intéressé à un des mots latins les plus obscurs, consectetur, extrait d'un passage du Lorem Ipsum, et en étudiant tous les usages de ce mot dans la littérature classique, découvrit la source incontestable du Lorem Ipsum. Il provient en fait des sections 1.10.32 et 1.10.33 du "De Finibus Bonorum et Malorum" (Des Suprêmes Biens et des Suprêmes Maux) de Cicéron. Cet ouvrage, très populaire pendant la Renaissance, est un traité sur la théorie de l'éthique. Les premières lignes du Lorem Ipsum, "Lorem ipsum dolor sit amet...", proviennent de la section 1.10.32.</div><br></br>
-<div><strong>Why</strong><br></br>
-Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</div><br></br>
-
-<div className="imgJSPresentation">
-
-<img src={new1F} alt="tennis de table" className="imgNew1F"/></div>
-</div>
-<div className="lien suite">lien vers l'article</div>
-
-
+        <div className="title">Les news de la Fondation</div>
+   
+    <div className="container">
+    
+      <div className="square">
+     <img src={new1F} alt="tennis de table" className="mask"/>
+     <div className="h1">Is Apple a Design Company?</div>
+        <p>Apple is more than a tech company; it became a culture unto itself, a passion of most of people and the birthplace of the world’s most revolutionized products.</p>
+        
+     <div><a href="https://medium.com/@laheshk/is-apple-a-design-company-f5c83514e261" target="_" className="button">Lire la suite</a></div>
+      </div>
+      
+      
+      
+     
+      
+        
+      
+      
+    </div>
+    
+    
+  
+              
     </div>)
 }
 
--- src/components/Presentation/index.js
+++ src/components/Presentation/index.js
@@ -1,27 +1,39 @@
 import article from '../../assets/article.png';
 import './style.scss';
+import '../../styles/commun.scss';
 
 function Presentation(){
-    return (
-        <div className='containerPresentation'><div className="titlePresentation">Présentation de la fondation</div>
-            <div className="containerPresentationContenu">
+    return (     
+  <div className="containerPrésentation">
+    <div className="title">Présentation de la fondation</div>
+     
+                    <div className="img-card iCard-style1">
+                        <div className="card-content">
+                            <div className="card-image">
+                               
+                                <img src={article} alt="img de JS" className="imgJS"/>
+                            </div>
+                            
+                            <div className="card-text">
+                                <h2>Présentation de Jacques Secrétin</h2>
+                                <p>
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Image by <a href="https://pixabay.com/users/Hans-2/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3461451" >Hans Braxmeier</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3461451" >Pixabay</a>
+                                </p>
+                            </div>
+                            
+                        </div>
+                        
+                        <div class="card-link">
+                            <a href="# " title="Read Full"><span>Lire la suite</span></a>
+                        </div>
+                    </div>                    
+              
+
+  </div>
 
 
-<div className="imgJSPresentation">
-<img src={article} alt="img de JS" className="imgJS"/></div>
 
-<div className="containerTxt">
-<div><strong> Qu'est-ce que le Lorem Ipsum?</strong><br></br>
-Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</div><br></br>
-<div>
-<strong>Pourquoi l'utiliser?</strong><br></br>
-On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L'avantage du Lorem Ipsum sur un texte générique comme 'Du texte. Du texte. Du texte.' est qu'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour 'Lorem Ipsum' vous conduira vers de nombreux sites qui n'en sont encore qu'à leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement (histoire d'y rajouter de petits clins d'oeil, voire des phrases embarassantes).</div><br></br>
 
-<div>
-<strong>D'où vient-il?</strong><br></br>
-Contrairement à une opinion répandue, le Lorem Ipsum n'est pas simplement du texte aléatoire. Il trouve ses racines dans une oeuvre de la littérature latine classique datant de 45 av. J.-C., le rendant vieux de 2000 ans. Un professeur du Hampden-Sydney College, en Virginie, s'est intéressé à un des mots latins les plus obscurs, consectetur, extrait d'un passage du Lorem Ipsum, et en étudiant tous les usages de ce mot dans la littérature classique, découvrit la source incontestable du Lorem Ipsum. Il provient en fait des sections 1.10.32 et 1.10.33 du "De Finibus Bonorum et Malorum" (Des Suprêmes Biens et des Suprêmes Maux) de Cicéron. Cet ouvrage, très populaire pendant la Renaissance, est un traité sur la théorie de l'éthique. Les premières lignes du Lorem Ipsum, "Lorem ipsum dolor sit amet...", proviennent de la section 1.10.32.</div><br></br>
-     </div>  </div> 
-     <div className="lien presentation "> Plus de new....</div></div>
-    )
+)
 }
 export default Presentation;
