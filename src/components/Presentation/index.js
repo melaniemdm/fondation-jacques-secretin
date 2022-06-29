042a7aaf0cea64e5605fe2a25cb6414a59756a88
melaniemdm <melanieborja.pro@gmail.com>
2022-06-29 10:53:26 +0200

    add(js&css) lien fake fix(js & css) presentation

--- src/components/Presentation/index.js
+++ src/components/Presentation/index.js
@@ -3,8 +3,8 @@ import './style.scss';
 
 function Presentation(){
     return (
-        <div ><div className="title">Présentation de la fondation</div>
-            <div className="containerPresentation">
+        <div className='containerPresentation'><div className="title">Présentation de la fondation</div>
+            <div className="containerPresentationContenu">
 
 
 <div className="imgJSPresentation">
@@ -20,7 +20,8 @@ On sait depuis longtemps que travailler avec du texte lisible et contenant du se
 <div>
 <strong>D'où vient-il?</strong><br></br>
 Contrairement à une opinion répandue, le Lorem Ipsum n'est pas simplement du texte aléatoire. Il trouve ses racines dans une oeuvre de la littérature latine classique datant de 45 av. J.-C., le rendant vieux de 2000 ans. Un professeur du Hampden-Sydney College, en Virginie, s'est intéressé à un des mots latins les plus obscurs, consectetur, extrait d'un passage du Lorem Ipsum, et en étudiant tous les usages de ce mot dans la littérature classique, découvrit la source incontestable du Lorem Ipsum. Il provient en fait des sections 1.10.32 et 1.10.33 du "De Finibus Bonorum et Malorum" (Des Suprêmes Biens et des Suprêmes Maux) de Cicéron. Cet ouvrage, très populaire pendant la Renaissance, est un traité sur la théorie de l'éthique. Les premières lignes du Lorem Ipsum, "Lorem ipsum dolor sit amet...", proviennent de la section 1.10.32.</div><br></br>
-     </div>  </div> </div>
+     </div>  </div> 
+     <div className="lien presentation "> Plus de new....</div></div>
     )
 }
 export default Presentation;
