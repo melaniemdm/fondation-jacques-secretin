5b3113f44e4d5013f21a1010fcfc0c62e4c27046
melaniemdm <melanieborja.pro@gmail.com>
2022-06-29 11:31:00 +0200

    add(img) assets mission add(js) title mission

--- src/components/Mission/index.js
+++ src/components/Mission/index.js
@@ -2,7 +2,11 @@ import './style.scss'
 
 function Mission(){
     return(<div>
-
+<div>Faire connaite le Tennins de table aux plus jeunes</div>
+<div>Créer des vocations</div> 
+<div>Renforcer la solidarité et l'esprit d'équipe</div>
+<div>Faire rêver les enfants </div>
+<div>Augmlenter l'épanouissement des grands et petits</div>
     </div>)
 }
 export default Mission;
