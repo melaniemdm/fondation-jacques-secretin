9ce07e39e6eec9a519ba240814481a3cb52f79a1
melaniemdm <melanieborja.pro@gmail.com>
2022-06-30 11:05:09 +0200

    fix(js & css) presentation

--- src/components/Presentation/index.js
+++ src/components/Presentation/index.js
@@ -1,38 +1,25 @@
-import article from '../../assets/article.png';
+//import article from '../../assets/article.png';
 import './style.scss';
 import '../../styles/commun.scss';
 
 function Presentation(){
     return (     
-        <div className='containerNews'>
-        <div className="title">Présentation de la fondation</div>
-   
-    <div className="container">
-    
-      <div className="square">
-     <img src={article} alt="tennis de table" className="mask"/>
-     <div className="h1">Is Apple a Design Company?</div>
-        <p>Apple is more than a tech company; it became a culture unto itself, a passion of most of people and the birthplace of the world’s most revolutionized products.</p>
-        
-     <div><a href="https://medium.com/@laheshk/is-apple-a-design-company-f5c83514e261" target="_" className="button">Lire la suite</a></div>
-      </div>
-      
-      
-      
-     
-      
-        
-      
-      
-    </div>
-    
-    
   
-              
-    </div>
-
-
-
+<div className='containerPresentation'>
+  <div className="title">Présentation</div>
+<div class="cardPresentation">
+  <div class="img-boxPresentation">
+    <img src="https://images.pexels.com/photos/12258844/pexels-photo-12258844.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className="imgPresentationFondation"/>
+  </div>
+  <div class="contentPresentationFondation">
+    <h3 className="titlePresentationFondation">Création de la fondation</h3>
+    <div className="txtPresentationFondation">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, nihil autem. Voluptates magni dolorum minus? Necessitatibus minus vero numquam, ex placeat provident officia explicabo, accusantium sit molestiae excepturi earum ab tenetur soluta a deserunt. Qui.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, nihil autem. Voluptates magni dolorum minus? Necessitatibus minus vero numquam, ex placeat provident officia explicabo, accusantium sit molestiae excepturi earum ab tenetur soluta a deserunt. Qui.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, nihil autem. Voluptates magni dolorum minus? Necessitatibus minus vero numquam, ex placeat provident officia explicabo, accusantium sit molestiae excepturi earum ab tenetur soluta a deserunt. Qui.</div>
+    
+  </div>
+</div>
+</div>
 
 )
 }
