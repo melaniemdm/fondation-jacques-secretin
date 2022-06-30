ea5758a3a1b49940b1f4f08dfac50d8432e23863
melaniemdm <melanieborja.pro@gmail.com>
2022-06-30 21:19:25 +0200

    fix(css) background all feature(js) add separator banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -6,13 +6,13 @@ import './style.scss';
 function Banner(props){
   
     return (
-      <div className="slide-container">
+      <div className="slide-container"> <div className="separator"></div>
         <Slide   duration={1000} transitionDuration={3000}  cssClass='slideShow'  arrows={false} slidesToShow={1}>
           {
             props.data.map((each, index) => <img key={index} style={{width: "100%"}} src={process.env.PUBLIC_URL +'/'+ each} alt="images Jacques Secrétin " className="slideShowImg"/>)
-          }
+          } 
         </Slide>
-       
+     
       </div>
     )
 }
