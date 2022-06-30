88bbc33cbd7fb19c5896173ca6faee43f8fd46dd
melaniemdm <melanieborja.pro@gmail.com>
2022-06-30 15:41:43 +0200

    fix(css) missions

---
+++ src/datas/missions.js
@@ -0,0 +1,10 @@
+export const missions=
+{
+    'titleMissions':[
+        `aire connaite le Tennins de table aux plus jeunes`,
+      `Créer des vocations`,  
+      `Renforcer la solidarité et l'esprit d'équipe`,
+      `Faire rêver les enfants`,
+      `Augmlenter l'épanouissement des grands et petits`
+    ]
+}
